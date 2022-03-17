8c44d013030c697ce77c6ff302f910a7256f79c5
Moses Levin <moses.levin14@gmail.com>
2022-03-17 10:47:34 +0100

    test_myFirstNpmCow

---
+++ index.js
@@ -0,0 +1,14 @@
+const userInfo = require("./information");
+
+console.log(userInfo);
+
+
+var cowsay = require("cowsay");
+
+console.log(cowsay.think({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
+
+// or cowsay.think()
---
+++ information.js
@@ -0,0 +1,9 @@
+let myObject = {
+  name: "Moses",
+  campus: "Baerlin",
+};
+module.exports = {
+  name: myObject.name,
+
+  campus: myObject.campus,
+};
